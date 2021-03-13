d1060881a35cdd45cde4bd58031b71e06d1f605e
xebec19 <thakur190420@gmail.com>
2021-03-13 11:45:04 +0530

    save

--- src/App.js
+++ src/App.js
@@ -3,8 +3,13 @@ import {
   InstantSearch,
   SearchBox,
   Hits,
-  Highlight
-} from 'react-instantsearch/dom'
+  Highlight,
+  Stats,
+  SortBy,
+  Pagination,
+  RefinementList,
+  Menu
+} from 'react-instantsearch-dom'
 
 const Hit = ({hit}) => 
 <div className="hit">
@@ -15,20 +20,45 @@ const Hit = ({hit}) =>
 <div className="hit-price">
 ${hit.price}
 </div>
+
 <div className="hit-name">
-<Highlight attributeName="name" hit="hit"/>
+<Highlight attributeName="name" hit={hit}/>
 </div>
+
+<div className="hit-description">
+<Highlight attributeName="description" hit={hit}/>
+</div>
+
 </div>
 </div>
 
 const Sidebar = () => 
 <div className="sidebar">
-
+<h5>Category</h5>
+<RefinementList attributeName="categories"/>
+<h5>Brand</h5>
+<RefinementList attributeName="brand" withSearchBox/>
+<h5>Type</h5>
+<Menu attribute="type"/>
 </div>
 
 const Content = () => 
 <div className="content">
+<div className="Info">
+<Stats/>
+<SortBy
+defaultRefinement="instant_search"
+items={[
+  {value:"instant_search", label:"Most Relevent"},
+  {value:"instant-search_price_asc", label:"Lowest Price"},
+  {value:"instant_search_price_desc", label:"Highest Price"}
+]}
+/>
+</div>
 <Hits hitComponent={Hit}/>
+<div className="pagination">
+<Pagination showList/>
+</div>
 </div>
 
 class App extends Component{
